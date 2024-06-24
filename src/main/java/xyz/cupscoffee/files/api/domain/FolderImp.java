5933d6ed8b58eddabd86536330760e280bdba85e
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-06-24 18:48:58 -0500

    FEAT: Add a list of folders on Folder

--- src/main/java/xyz/cupscoffee/files/api/domain/FolderImp.java
+++ src/main/java/xyz/cupscoffee/files/api/domain/FolderImp.java
@@ -10,58 +10,64 @@ import java.util.List;
 import java.util.Map;
 
 public class FolderImp implements Folder {
-	private String name;
-	private List<File> files = new ArrayList<>();
-	private Date created;
-	private Date lastModified;
-	private long size;
-	private Path path;
-	private Map<String, String> meta;
+    private String name;
+    private List<File> files = new ArrayList<>();
+    private List<Folder> folders = new ArrayList<>();
+    private Date created;
+    private Date lastModified;
+    private long size;
+    private Path path;
+    private Map<String, String> meta;
 
-	public FolderImp(String name, List<File> files, Date created,
-			Date lastModified,
-			long size, Path path, Map<String, String> meta) {
-		this.name = name;
-		this.files = files;
-		this.created = created;
-		this.lastModified = lastModified;
-		this.size = size;
-		this.path = path;
-		this.meta = meta;
-	}
+    public FolderImp(String name, List<File> files, Date created,
+            Date lastModified,
+            long size, Path path, Map<String, String> meta) {
+        this.name = name;
+        this.files = files;
+        this.created = created;
+        this.lastModified = lastModified;
+        this.size = size;
+        this.path = path;
+        this.meta = meta;
+    }
 
-	@Override
-	public String getName() {
-		return name;
-	}
+    @Override
+    public String getName() {
+        return name;
+    }
 
-	@Override
-	public List<File> getFiles() {
-		return files;
-	}
+    @Override
+    public List<File> getFiles() {
+        return files;
+    }
 
-	@Override
-	public Date getCreatedDate() {
-		return this.created;
-	}
+    @Override
+    public Date getCreatedDate() {
+        return this.created;
+    }
 
-	@Override
-	public Date getLastModifiedDate() {
-		return this.lastModified;
-	}
+    @Override
+    public Date getLastModifiedDate() {
+        return this.lastModified;
+    }
 
-	@Override
-	public long getSize() {
-		return this.size;
-	}
+    @Override
+    public long getSize() {
+        return this.size;
+    }
 
-	@Override
-	public Path getPath() {
-		return this.path;
-	}
+    @Override
+    public Path getPath() {
+        return this.path;
+    }
 
-	@Override
-	public Map<String, String> getOtherMeta() {
-		return this.meta;
-	}
+    @Override
+    public Map<String, String> getOtherMeta() {
+        return this.meta;
+    }
+
+    @Override
+    public List<Folder> getFolders() {
+        return this.folders;
+    }
 }
--- src/main/java/xyz/cupscoffee/files/api/interfaces/Folder.java
+++ src/main/java/xyz/cupscoffee/files/api/interfaces/Folder.java
@@ -4,5 +4,8 @@ import java.util.List;
 
 public interface Folder extends Metadata {
     String getName();
+
     List<File> getFiles();
+
+    List<Folder> getFolders();
 }
