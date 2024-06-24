c901de5c2c1296478cb91323b69c16d61bef293e
Luis Bazan <luis@bazan.pe>
2024-06-24 17:12:03 -0500

    Update exeption in driver read

--- src/main/java/xyz/cupscoffee/files/driver/CupsOfCoffeeDriver.java
+++ src/main/java/xyz/cupscoffee/files/driver/CupsOfCoffeeDriver.java
@@ -1,12 +1,14 @@
 package xyz.cupscoffee.files.driver;
 
 import xyz.cupscoffee.files.api.interfaces.Disk;
+import xyz.cupscoffee.files.exception.InvalidFormatFile;
 
 import java.io.FileInputStream;
 
 public class CupsOfCoffeeDriver implements SavDriver {
     @Override
-    public Disk[] readSavFile(FileInputStream fileInputStream) {
+    public Disk[] readSavFile(FileInputStream fileInputStream) throws
+            InvalidFormatFile {
         // TODO: Unimplemented method
         throw new UnsupportedOperationException("Unimplemented method 'readSavFile'");
     }
--- src/main/java/xyz/cupscoffee/files/driver/SavDriver.java
+++ src/main/java/xyz/cupscoffee/files/driver/SavDriver.java
@@ -1,9 +1,11 @@
 package xyz.cupscoffee.files.driver;
 
 import xyz.cupscoffee.files.api.interfaces.Disk;
+import xyz.cupscoffee.files.exception.InvalidFormatFile;
 
 import java.io.FileInputStream;
 
 public interface SavDriver {
-    Disk[] readSavFile(FileInputStream fileInputStream);
+    Disk[] readSavFile(FileInputStream fileInputStream) throws
+            InvalidFormatFile;
 }
