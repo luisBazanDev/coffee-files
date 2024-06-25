87046f170a15729983bc262e0a181f1cadd8dabc
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-06-24 23:53:15 -0500

    FEAT: Add body of CupsOfCoffeeDriver

--- src/main/java/xyz/cupscoffee/files/api/driver/teams/CupsOfCoffeeDriver.java
+++ src/main/java/xyz/cupscoffee/files/api/driver/teams/CupsOfCoffeeDriver.java
@@ -2,9 +2,14 @@ package xyz.cupscoffee.files.api.driver.teams;
 
 import xyz.cupscoffee.files.api.driver.SavDriver;
 import xyz.cupscoffee.files.api.exception.InvalidFormatFileException;
+import xyz.cupscoffee.files.api.implementation.BasicSavFile;
+import xyz.cupscoffee.files.api.Disk;
 import xyz.cupscoffee.files.api.SavFile;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 
 /**
  * CupsOfCoffee implementation of the SavDriver interface.
@@ -12,7 +17,28 @@ import java.io.FileInputStream;
 public class CupsOfCoffeeDriver implements SavDriver {
     @Override
     public SavFile readSavFile(FileInputStream fileInputStream) throws InvalidFormatFileException {
-        // TODO: Unimplemented method
-        throw new UnsupportedOperationException("Unimplemented method 'readSavFile'");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream));
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (Exception e) {
+            throw new InvalidFormatFileException("Error reading file");
+        }
+
+        String data = sb.toString();
+
+        return new BasicSavFile(getClass().getSimpleName(), getDisk(data), getMetadata(data));
+    }
+
+    private Disk[] getDisk(String data) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    private HashMap<String, String> getMetadata(String data) {
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 }
