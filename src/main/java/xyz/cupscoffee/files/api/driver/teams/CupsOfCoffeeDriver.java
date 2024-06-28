f9d58e10c89cc0c8ba2e8ec86b61897da0ece503
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-06-27 21:12:27 -0500

    FIX: Change from FileInputStream to InputStream

--- src/main/java/xyz/cupscoffee/files/api/driver/teams/CupsOfCoffeeDriver.java
+++ src/main/java/xyz/cupscoffee/files/api/driver/teams/CupsOfCoffeeDriver.java
@@ -3,11 +3,15 @@ package xyz.cupscoffee.files.api.driver.teams;
 import xyz.cupscoffee.files.api.driver.SavDriver;
 import xyz.cupscoffee.files.api.exception.InvalidFormatFileException;
 import xyz.cupscoffee.files.api.implementation.SimpleSavStructure;
+import xyz.cupscoffee.files.api.util.StringCompresor;
 import xyz.cupscoffee.files.api.Disk;
+import xyz.cupscoffee.files.api.File;
+import xyz.cupscoffee.files.api.Folder;
 import xyz.cupscoffee.files.api.SavStructure;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
@@ -20,8 +24,8 @@ import java.util.HashMap;
  */
 public class CupsOfCoffeeDriver implements SavDriver {
     @Override
-    public SavStructure readSavFile(FileInputStream fileInputStream) throws InvalidFormatFileException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream));
+    public SavStructure readSavFile(InputStream inputStream) throws InvalidFormatFileException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
         StringBuilder sb = new StringBuilder();
         String line;
@@ -33,12 +37,22 @@ public class CupsOfCoffeeDriver implements SavDriver {
             throw new InvalidFormatFileException("Error reading file");
         }
 
-        String data = sb.toString();
+        String data = StringCompresor.decompress(sb.toString());
+        String header = getClass().getSimpleName();
+        header = header.substring(0, header.length() - 6);
 
-        return new SimpleSavStructure(getClass().getSimpleName(), getDisks(data), getMetadata(data));
+        return new SimpleSavStructure(header, getDisks(data), getMetadata(data));
     }
 
     private Disk[] getDisks(String data) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    private File getFile(String data) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    private Folder getFolder(String data) {
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
