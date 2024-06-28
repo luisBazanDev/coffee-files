75645129c10c413ecec68dd2c8805e6bc72256b7
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-06-28 14:47:28 -0500

    FEAT: Add simple implementation of Metadata

---
+++ src/main/java/xyz/cupscoffee/files/api/implementation/SimpleMetadata.java
@@ -0,0 +1,58 @@
+package xyz.cupscoffee.files.api.implementation;
+
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Objects;
+
+import xyz.cupscoffee.files.api.Metadata;
+
+/**
+ * Simple implementation of the {@link Metadata} interface.
+ */
+public class SimpleMetadata implements Metadata {
+    private LocalDateTime created;
+    private LocalDateTime lastModified;
+    private long size;
+    private Path path;
+    private Map<String, String> meta;
+
+    public SimpleMetadata(LocalDateTime created, LocalDateTime lastModified, long size, Path path,
+            Map<String, String> meta) {
+        Objects.requireNonNull(created, "Created cannot be null");
+        Objects.requireNonNull(lastModified, "Last modified cannot be null");
+        Objects.requireNonNull(path, "Path cannot be null");
+        Objects.requireNonNull(meta, "Metadata cannot be null");
+
+        this.created = created;
+        this.lastModified = lastModified;
+        this.size = size;
+        this.path = path;
+        this.meta = meta;
+    }
+
+    @Override
+    public LocalDateTime getCreatedDateTime() {
+        return this.created;
+    }
+
+    @Override
+    public LocalDateTime getLastModifiedDateTime() {
+        return this.lastModified;
+    }
+
+    @Override
+    public long getSize() {
+        return this.size;
+    }
+
+    @Override
+    public Path getPath() {
+        return this.path;
+    }
+
+    @Override
+    public Map<String, String> getOtherMeta() {
+        return this.meta;
+    }
+}
