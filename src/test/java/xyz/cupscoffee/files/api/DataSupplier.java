912e1124b042e1c552fb804f0b0235ce6fe09e2d
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-06-29 12:28:11 -0500

    FEAT: Complete driver tests

--- src/test/java/xyz/cupscoffee/files/api/DataSupplier.java
+++ src/test/java/xyz/cupscoffee/files/api/DataSupplier.java
@@ -3,6 +3,7 @@ package xyz.cupscoffee.files.api;
 import java.nio.ByteBuffer;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.Base64;
 import java.util.LinkedList;
 import java.util.List;
@@ -22,44 +23,49 @@ public class DataSupplier {
         Map<String, String> metadata = new java.util.HashMap<>();
         metadata.put("author", "Elder");
 
+        Long createdEpochs = 1719577849L;
+        Long lastModifiedEpochs = 1719577849L;
+        LocalDateTime created = LocalDateTime.ofEpochSecond(createdEpochs, 0, ZoneOffset.of("Z"));
+        LocalDateTime lastModified = LocalDateTime.ofEpochSecond(lastModifiedEpochs, 0, ZoneOffset.of("Z"));
+
         SimpleFile simpleFile = new SimpleFile("file.js",
                 fileContentBuffer,
-                LocalDateTime.now(),
-                LocalDateTime.now(),
+                created,
+                lastModified,
                 Path.of("A:\\directory\\other_directory\\file.js"),
                 metadata);
         SimpleFolder simpleFolder1 = new SimpleFolder("other_directory",
                 List.of(simpleFile),
                 new LinkedList<>(),
-                LocalDateTime.now(),
-                LocalDateTime.now(),
+                created,
+                lastModified,
                 Path.of("A:\\directory\\other_directory"),
                 metadata);
         SimpleFolder simpleFolder2 = new SimpleFolder("directory",
                 new java.util.LinkedList<>(),
                 new LinkedList<>(),
-                LocalDateTime.now(),
-                LocalDateTime.now(),
+                created,
+                lastModified,
                 Path.of("A:\\directory\\directory"),
                 metadata);
         SimpleFolder simpleFolder3 = new SimpleFolder("directory",
                 new java.util.LinkedList<>(),
                 List.of(simpleFolder1, simpleFolder2),
-                LocalDateTime.now(),
-                LocalDateTime.now(),
+                created,
+                lastModified,
                 Path.of("A:\\directory"),
                 metadata);
         SimpleFolder root = new SimpleFolder("",
                 new java.util.LinkedList<>(),
                 List.of(simpleFolder3),
-                LocalDateTime.now(),
-                LocalDateTime.now(),
+                created,
+                lastModified,
                 Path.of("A:\\"),
                 metadata);
 
         SimpleDisk simpleDisk = new SimpleDisk("A",
                 root,
-                root.getSize() * 3,
+                20000,
                 new java.util.HashMap<>());
 
         SimpleSavStructure expectedSavFileStructure = new SimpleSavStructure(
--- src/test/java/xyz/cupscoffee/files/api/SavReaderTest.java
+++ src/test/java/xyz/cupscoffee/files/api/SavReaderTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.FileInputStream;
 import java.nio.file.Path;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,15 +20,73 @@ public class SavReaderTest {
     void testCupsOfCoffeeDriver() {
         savDriver = new CupsOfCoffeeDriver();
 
-        java.io.File file = new java.io.File(resourcesPath.toFile(), "tcoc.sav");
+        SavStructure actualSavFileStructure = getSavStructure("tcoc.sav");
 
-        SavStructure actualSavFileStructure = null;
+        SavStructure expectedSavFileStructure = DataSupplier.getSavStructure();
+        testSavStructure(expectedSavFileStructure, actualSavFileStructure);
+    }
+
+    private SavStructure getSavStructure(String file) {
+        java.io.File savFile = new java.io.File(resourcesPath.toFile(), file);
+
+        SavStructure savStructure = null;
         try {
-            actualSavFileStructure = savDriver.readSavFile(new FileInputStream(file));
+            savStructure = savDriver.readSavFile(new FileInputStream(savFile));
         } catch (Exception e) {
             fail("Error reading file " + e.getMessage());
         }
 
-        assertEquals(DataSupplier.getSavStructure(), actualSavFileStructure);
+        return savStructure;
+    }
+
+    private void testSavStructure(SavStructure expectedSavStructure, SavStructure actualSavStructure) {
+        assertEquals(expectedSavStructure.getDisks().length, actualSavStructure.getDisks().length);
+
+        for (int i = 0; i < expectedSavStructure.getDisks().length; i++) {
+            testDisk(expectedSavStructure.getDisks()[i], actualSavStructure.getDisks()[i]);
+        }
+    }
+
+    private void testDisk(Disk expectedDisk, Disk actualDisk) {
+        assertEquals(expectedDisk.getName(), actualDisk.getName());
+        assertEquals(expectedDisk.getLimitSize(), actualDisk.getLimitSize());
+        assertEquals(expectedDisk.getOccupiedSize(), actualDisk.getOccupiedSize());
+        testFolder(expectedDisk.getRootFolder(), actualDisk.getRootFolder());
+        testSimpleMetadata(expectedDisk.getMetadata(), actualDisk.getMetadata());
+    }
+
+    private void testFolder(Folder expectedFolder, Folder actualFolder) {
+        assertEquals(expectedFolder.getName(), actualFolder.getName());
+        assertEquals(expectedFolder.getFiles().size(), actualFolder.getFiles().size());
+        assertEquals(expectedFolder.getFolders().size(), actualFolder.getFolders().size());
+
+        for (int i = 0; i < expectedFolder.getFiles().size(); i++) {
+            testFile(expectedFolder.getFiles().get(i), actualFolder.getFiles().get(i));
+        }
+
+        for (int i = 0; i < expectedFolder.getFolders().size(); i++) {
+            testFolder(expectedFolder.getFolders().get(i), actualFolder.getFolders().get(i));
+        }
+
+        testMetadata(expectedFolder, actualFolder);
+    }
+
+    private void testFile(File expectedFile, File actualFile) {
+        assertEquals(expectedFile.getName(), actualFile.getName());
+        assertEquals(expectedFile.getContent(), actualFile.getContent());
+        testMetadata(expectedFile, actualFile);
+    }
+
+    private void testMetadata(Metadata expectedMetadata, Metadata actualMetadata) {
+        assertEquals(expectedMetadata.getCreatedDateTime(), actualMetadata.getCreatedDateTime());
+        assertEquals(expectedMetadata.getLastModifiedDateTime(), actualMetadata.getLastModifiedDateTime());
+        assertEquals(expectedMetadata.getSize(), actualMetadata.getSize());
+        assertEquals(expectedMetadata.getPath(), actualMetadata.getPath());
+        testSimpleMetadata(expectedMetadata.getOtherMetadata(), actualMetadata.getOtherMetadata());
+    }
+
+    private void testSimpleMetadata(Map<String, String> expectedSimpleMetadata,
+            Map<String, String> actualSimpleMetadata) {
+        assertEquals(expectedSimpleMetadata.size(), actualSimpleMetadata.size());
     }
 }
